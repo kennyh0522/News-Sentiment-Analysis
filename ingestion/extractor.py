72d55079f65e0532837e6b3a5884f58c017deb24
kenny <152108677+kennyh0522@users.noreply.github.com>
2026-06-14 14:09:48 -0500

    Create extractor.py

---
+++ ingestion/extractor.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import date, timedelta
+import os
+from dotenv import load_dotenv
+
+
+def fetch_articles():
+    load_dotenv()
+    YESTERDAY = date.today() - timedelta(days = 1)
+    NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+    
+    url = 'https://newsapi.org/v2/everything?'
+    params = {
+        'q': 'AI OR "Computer Science" OR "Machine Learning"',
+        'from': YESTERDAY,                
+        'sortBy': 'publishedAt',                
+        'apiKey': NEWS_API_KEY,        
+        'language': 'en'
+    }
+
+    r = requests.get(url, params= params)
+    
+    if r.ok:
+        return r.json()
+    raise RuntimeError("Unable to fetch articles")
